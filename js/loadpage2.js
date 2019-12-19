3b06d0398eb5c534e7a347c1e7396a7773dc0e60
brownfieldbrown <55472237+brownfieldbrown@users.noreply.github.com>
2019-12-19 11:28:35 -0600

    Update loadpage2.js

--- js/loadpage2.js
+++ js/loadpage2.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
 
-	setTimeout(() => { window.location.replace("Hello_mainscreen.html"); }, 30000); // "3000" is the amount of time before the next page loads in milliseconds.
+	setTimeout(() => { window.location.replace("hello_mainscreen.html"); }, 30000); // "3000" is the amount of time before the next page loads in milliseconds.
 
 });
